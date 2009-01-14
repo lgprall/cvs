47396a22d68b4de0f44ba2d6ac1b477a1466922c
larry <larry>
2009-01-14 13:54:07 +0000

    Removed kiesa specific handling; now done via ssh ProxyCommand

--- doall/chk_hosts.sh
+++ doall/chk_hosts.sh
@@ -2,8 +2,7 @@
 
 CMD=$@
 
-HOSTS="csi sato glory entourage barret firman lightning2 lorne ocean12 eko xander ocean11 fightclub klitschko cuttingclass";
-#HOSTS="csi sato glory entourage barret firman lightning2 lorne ocean12 eko xander ocean11 fightclub klitschko cuttingclass beingjohn";
+HOSTS="csi sato glory entourage kiesa barret firman lightning2 lorne ocean12 eko xander ocean11 fightclub klitschko cuttingclass beingjohn";
 
 for host in $HOSTS; do echo "=====> $host"; ssh $host "
 . /etc/sf/ims.conf
@@ -11,6 +10,3 @@ FILE=$SF_ETC_ROOT_PATH/etc/sf/PM.conf
 eval $CMD
 echo
 "; done 2>/dev/null
-
-ssh firman /usr/local/bin/chk_kiesa.sh "$CMD"
-
