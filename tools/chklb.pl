22090a4e3755e12f4576c4ab60b6473d76880c8d
larry <larry>
2009-03-14 02:50:19 +0000

    Added "select" and "discard" options. Added "output raw" and "output full" options. Cleaned up usage statement. Now show checked packets of total packets. Now provide for ipv6.
    (May have problem with ipv6 icmp...)

--- tools/chklb.pl
+++ tools/chklb.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# $Id$
+# $Id: chklb.pl,v 1.2 2009/03/12 21:36:00 larry Exp $
 
 use strict;
 use warnings;
@@ -8,28 +8,38 @@ use warnings;
 use Getopt::Long;
 
 (my $Cmd = $0 ) =~ s@^.*/@@;
+(my $BCmd = $Cmd) =~ s/.pl//;
 my $Usage = <<USAGE ;
 
 Usage:
-$Cmd [--proto tcp|UdP|<prt> ...] [--exclude TCp|udP|<prt> ...] <input_file_1>
-	<input_file_2> [ output_file ]
+$Cmd [--proto|--exclude tcp|UdP|<prt> ...] [--select regex] [--discard regex]
+	[--output raw|full] input_file_1 input_file_2 [ output_file ]
 
-$Cmd [-p tcp|UdP|<prt> ...] [-x TCp|udP|<prt> ...] <input_file_1> <input_file_2>
-	[ output_file ]
-
-You can provide multiple prototypes to use or exclude.
+-p|--proto    Check this protocol; may use multiple times
+-x|--exclude  Exclude this procol; may use multiple times
 You cannot use both proto and exclude in the same command.
+-s|--selext   Examine only entries with this regex
+-d|--discard  Ignore all entries with this regex
+
+--output raw  Output the "IP:port -> IP:port Proto" string as seen.
+--output full Output the entire snort entries
+The default output is formatted.
 
 If no output file is specified, the detailed output will be written to
-	$Cmd<PID>.
+	$BCmd<PID>.
 
 USAGE
 
-my (@Proto, @Xclude);
+my (@Proto, @Xclude, $Select, $Discard, $Output);
 die $Usage unless GetOptions (
 	"proto=s"	=> \@Proto,
 	"x|exclude=s"	=> \@Xclude,
+	"select=s"	=> \$Select,
+	"discard=s"	=> \$Discard,
+	"output=s"	=> \$Output,
 );
+
+die $Usage if $Output && $Output ne "raw" && $Output ne "full";
 
 die $Usage if @Proto and @Xclude;
 
@@ -44,7 +54,7 @@ foreach (@Xclude) {
 my ($file1, $file2, $outfile) = @ARGV;
 die $Usage unless $file1 and $file2;
 
-$outfile ||= "chkconv$$";
+$outfile ||= "$BCmd$$";
 
 if ( ! -r $file1 or ! -r $file2 ) {
 	print "\nNeed readable files for args\n";
@@ -54,65 +64,101 @@ if ( ! -r $file1 or ! -r $file2 ) {
 open IN1, "<$file1" or die "Couldn't open $file1\n";
 open IN2, "<$file2" or die "Couldn't open $file2\n";
 
+print "Loading...\n";
+
 my ($con1, $con2);
 { local $/; $con1 = <IN1>; $con2 = <IN2>; }
+
+close IN1; close IN2;
+
 my @con1 = split /\n\n/, $con1;
-print scalar @con1, " connections in $file1\n";
+my $f1con = scalar @con1;
 
 my @con2 = split /\n\n/, $con2;
-print scalar @con2, " connections in $file2\n";
+my $f2con = scalar @con2;
 
 print "\nChecking for strays....\n";
 
+my ($s1, $s2);
+$s1 = $s2 = 0;
 my (%con1, %con2);
 foreach (@con1) {
-	next unless $_ =~ /((\d{1,3}\.){3}(\d{1,3})(:\d+)? -> \S+)\s+(\S+)/;
-        next if @Proto and not defined $Proto{$5};
-	next if defined $Xclude{$5};
+        next if $Select && $_ !~ /$Select/;
+	next if $Discard && $_ =~ /$Discard/;
+	next unless $_ =~ /(((\d{1,3}\.){3}(\d{1,3})|([[:xdigit:]]{4}:){7}([[:xdigit:]]{4}))(:\d+)? -> \S+)\s+(\S+)/;
+        next if @Proto and not defined $Proto{$8};
+	next if defined $Xclude{$8};
+	$con1{$1}{FULL} = $_;
 	$con1{$1}{COUNT}++;
-	$con1{$1}{TYPE} = $5; 
-        
+	$con1{$1}{TYPE} = $8; 
+	$s1++;
 }
 
 foreach (@con2) {
-	next unless $_ =~ /((\d{1,3}\.){3}(\d{1,3})(:\d+)? -> \S+)\s+(\S+)/;
+        next if $Select && $_ =~ /$Select/;
+	next if $Discard && $_ =~ /$Discard/;
+	next unless $_ =~ /(((\d{1,3}\.){3}(\d{1,3})|([[:xdigit:]]{4}:){7}([[:xdigit:]]{4}))(:\d+)? -> \S+)\s+(\S+)/;
+        next if @Proto and not defined $Proto{$8};
+	next if defined $Xclude{$8};
+	$con2{$1}{FULL} = $_;
 	$con2{$1}{COUNT}++;
-	$con2{$1}{TYPE} = $5; 
+	$con2{$1}{TYPE} = $8; 
+	$s2++;
 }
 
-my ($srcIP, $srcPort, $NULL, $destIP, $destPort, $proto, $count1, $count2);
+print "Checking $s1 connections of $f1con in $file1\n";
+print "Checking $s2 connections of $f2con in $file2\n";
+
+my ($srcIP, $srcPort, $NULL, $dstIP, $dstPort, $proto, $count1, $count2);
 open OUT, ">$outfile";
 format OUT =
-@<<<<<<<<<<<<<< @<<<<   @<<<<<<<<<<<<<< @<<<<  @<<<  @####  @####
-$srcIP, $srcPort, $destIP, $destPort, $proto, $count1, $count2
+@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< @<<<<   @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< @<<<<  @<<<  @####  @####
+$srcIP, $srcPort, $dstIP, $dstPort, $proto, $count1, $count2
 .
 
 format OUT_TOP =
-   Source IP    Port       Dest IP      Port  Proto  Inst1  Inst2
-=============== =====   =============== ===== =====  =====  =====
+              Source IP                 Port                       Dest IP              Port  Proto  Inst1  Inst2
+======================================= =====   ======================================= ===== =====  =====  =====
 .
 
 my $fails;
 foreach ( sort keys %con1 ) {
         if ( defined $con2{$_} && $con1{$_}{TYPE} eq $con2{$_}{TYPE} ) {
-        ($srcIP, $srcPort, $NULL, $destIP, $destPort) =
-		split /[ :]/, $_;
-        if ($srcPort eq "->") {
-		$srcPort = 0;
-                $destIP = $NULL;
-                $destPort = 0;
-	}
-	$proto = $con1{$_}{TYPE};
-        ($count1, $count2) = ($con1{$_}{COUNT}, $con2{$_}{COUNT});
-#	print OUT "FAILURE $_ ($con1{$_}{TYPE}) : $con1{$_}{COUNT} / $con2{$_}{COUNT}\n";
-	write OUT;
-	$fails++;
+		my ($src, $dst);
+		($src, $NULL, $dst) = split /\s+/, $_;
+		unless ($src =~ /(.*):(\d+)/) {
+			$srcIP = $src;
+			$srcPort = 0;
+		} else {
+			($srcIP, $srcPort) = ($1, $2);
+		}
+		#if ( !$srcPort || $srcPort eq "->" ) {
+		#	$srcPort = 0;
+		#	$dstIP = $NULL;
+		#	$dstPort = 0;
+		#}
+		unless ($dst =~ /(.*):(\d+)/) {
+			$dstIP = $dst;
+			$dstPort = 0;
+		} else {
+			($dstIP, $dstPort) = ($1, $2);
+		}
+		$proto = $con1{$_}{TYPE};
+		($count1, $count2) = ($con1{$_}{COUNT}, $con2{$_}{COUNT});
+		if ( $Output && $Output eq "raw") {
+			print OUT "$_ ($con1{$_}{TYPE}) : $con1{$_}{COUNT} / $con2{$_}{COUNT}\n";
+		} elsif ( $Output && $Output eq "full") {
+			print OUT "$con1{$_}{FULL}      ====> $con1{$_}{COUNT}\n$con2{$_}{FULL}      ====> $con2{$_}{COUNT}\n\n\n";
+		} else {
+			write OUT;
+		}
+		$fails++;
 	}
 }
 close OUT;
 
 if ( $fails ) {
-	print "There were a total of $fails failures.  See $outfile for specifics\n";
+	print "There ", $fails > 1 ? "were" : "was" , " a total of $fails failure", $fails > 1 ? "s"  : "", ".  See $outfile for specifics\n";
 }
 else {
 	print "No common conversations in both instances\n";
