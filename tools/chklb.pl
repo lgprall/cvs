302dfc742cc425d76a86cbfb286480b19774ba6d
larry <larry>
2009-03-31 19:41:57 +0000

    Tidied up.

--- tools/chklb.pl
+++ tools/chklb.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
 
-# $Id: chklb.pl,v 1.2 2009/03/12 21:36:00 larry Exp $
+# $Id: chklb.pl,v 1.3 2009/03/14 02:50:19 larry Exp $
 
 use strict;
 use warnings;
 
 use Getopt::Long;
 
-(my $Cmd = $0 ) =~ s@^.*/@@;
-(my $BCmd = $Cmd) =~ s/.pl//;
+( my $Cmd  = $0 )   =~ s@^.*/@@;
+( my $BCmd = $Cmd ) =~ s/.pl//;
 my $Usage = <<USAGE ;
 
 Usage:
@@ -30,35 +30,36 @@ If no output file is specified, the detailed output will be written to
 
 USAGE
 
-my (@Proto, @Xclude, $Select, $Discard, $Output);
-die $Usage unless GetOptions (
-	"proto=s"	=> \@Proto,
-	"x|exclude=s"	=> \@Xclude,
-	"select=s"	=> \$Select,
-	"discard=s"	=> \$Discard,
-	"output=s"	=> \$Output,
-);
+my ( @Proto, @Xclude, $Select, $Discard, $Output );
+die $Usage
+  unless GetOptions(
+    "proto=s"     => \@Proto,
+    "x|exclude=s" => \@Xclude,
+    "select=s"    => \$Select,
+    "discard=s"   => \$Discard,
+    "output=s"    => \$Output,
+  );
 
 die $Usage if $Output && $Output ne "raw" && $Output ne "full";
 
 die $Usage if @Proto and @Xclude;
 
-my (%Proto, %Xclude);
+my ( %Proto, %Xclude );
 foreach (@Proto) {
-	$Proto{uc($_)}++;
+    $Proto{ uc($_) }++;
 }
 foreach (@Xclude) {
-	$Xclude{uc($_)}++;
+    $Xclude{ uc($_) }++;
 }
 
-my ($file1, $file2, $outfile) = @ARGV;
+my ( $file1, $file2, $outfile ) = @ARGV;
 die $Usage unless $file1 and $file2;
 
 $outfile ||= "$BCmd$$";
 
-if ( ! -r $file1 or ! -r $file2 ) {
-	print "\nNeed readable files for args\n";
-        die $Usage;
+if ( !-r $file1 or !-r $file2 ) {
+    print "\nNeed readable files for args\n";
+    die $Usage;
 }
 
 open IN1, "<$file1" or die "Couldn't open $file1\n";
@@ -66,10 +67,11 @@ open IN2, "<$file2" or die "Couldn't open $file2\n";
 
 print "Loading...\n";
 
-my ($con1, $con2);
+my ( $con1, $con2 );
 { local $/; $con1 = <IN1>; $con2 = <IN2>; }
 
-close IN1; close IN2;
+close IN1;
+close IN2;
 
 my @con1 = split /\n\n/, $con1;
 my $f1con = scalar @con1;
@@ -79,37 +81,41 @@ my $f2con = scalar @con2;
 
 print "\nChecking for strays....\n";
 
-my ($s1, $s2);
+my ( $s1, $s2 );
 $s1 = $s2 = 0;
-my (%con1, %con2);
+my ( %con1, %con2 );
 foreach (@con1) {
-        next if $Select && $_ !~ /$Select/;
-	next if $Discard && $_ =~ /$Discard/;
-	next unless $_ =~ /(((\d{1,3}\.){3}(\d{1,3})|([[:xdigit:]]{4}:){7}([[:xdigit:]]{4}))(:\d+)? -> \S+)\s+(\S+)/;
-        next if @Proto and not defined $Proto{$8};
-	next if defined $Xclude{$8};
-	$con1{$1}{FULL} = $_;
-	$con1{$1}{COUNT}++;
-	$con1{$1}{TYPE} = $8; 
-	$s1++;
+    next if $Select && $_ !~ /$Select/;
+    next if $Discard && $_ =~ /$Discard/;
+    next
+      unless $_ =~
+/(((\d{1,3}\.){3}(\d{1,3})|([[:xdigit:]]{4}:){7}([[:xdigit:]]{4}))(:\d+)? -> \S+)\s+(\S+)/;
+    next if @Proto and not defined $Proto{$8};
+    next if defined $Xclude{$8};
+    $con1{$1}{FULL} = $_;
+    $con1{$1}{COUNT}++;
+    $con1{$1}{TYPE} = $8;
+    $s1++;
 }
 
 foreach (@con2) {
-        next if $Select && $_ =~ /$Select/;
-	next if $Discard && $_ =~ /$Discard/;
-	next unless $_ =~ /(((\d{1,3}\.){3}(\d{1,3})|([[:xdigit:]]{4}:){7}([[:xdigit:]]{4}))(:\d+)? -> \S+)\s+(\S+)/;
-        next if @Proto and not defined $Proto{$8};
-	next if defined $Xclude{$8};
-	$con2{$1}{FULL} = $_;
-	$con2{$1}{COUNT}++;
-	$con2{$1}{TYPE} = $8; 
-	$s2++;
+    next if $Select  && $_ =~ /$Select/;
+    next if $Discard && $_ =~ /$Discard/;
+    next
+      unless $_ =~
+/(((\d{1,3}\.){3}(\d{1,3})|([[:xdigit:]]{4}:){7}([[:xdigit:]]{4}))(:\d+)? -> \S+)\s+(\S+)/;
+    next if @Proto and not defined $Proto{$8};
+    next if defined $Xclude{$8};
+    $con2{$1}{FULL} = $_;
+    $con2{$1}{COUNT}++;
+    $con2{$1}{TYPE} = $8;
+    $s2++;
 }
 
 print "Checking $s1 connections of $f1con in $file1\n";
 print "Checking $s2 connections of $f2con in $file2\n";
 
-my ($srcIP, $srcPort, $NULL, $dstIP, $dstPort, $proto, $count1, $count2);
+my ( $srcIP, $srcPort, $NULL, $dstIP, $dstPort, $proto, $count1, $count2 );
 open OUT, ">$outfile";
 format OUT =
 @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< @<<<<   @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< @<<<<  @<<<  @####  @####
@@ -123,43 +129,51 @@ format OUT_TOP =
 
 my $fails;
 foreach ( sort keys %con1 ) {
-        if ( defined $con2{$_} && $con1{$_}{TYPE} eq $con2{$_}{TYPE} ) {
-		my ($src, $dst);
-		($src, $NULL, $dst) = split /\s+/, $_;
-		unless ($src =~ /(.*):(\d+)/) {
-			$srcIP = $src;
-			$srcPort = 0;
-		} else {
-			($srcIP, $srcPort) = ($1, $2);
-		}
-		#if ( !$srcPort || $srcPort eq "->" ) {
-		#	$srcPort = 0;
-		#	$dstIP = $NULL;
-		#	$dstPort = 0;
-		#}
-		unless ($dst =~ /(.*):(\d+)/) {
-			$dstIP = $dst;
-			$dstPort = 0;
-		} else {
-			($dstIP, $dstPort) = ($1, $2);
-		}
-		$proto = $con1{$_}{TYPE};
-		($count1, $count2) = ($con1{$_}{COUNT}, $con2{$_}{COUNT});
-		if ( $Output && $Output eq "raw") {
-			print OUT "$_ ($con1{$_}{TYPE}) : $con1{$_}{COUNT} / $con2{$_}{COUNT}\n";
-		} elsif ( $Output && $Output eq "full") {
-			print OUT "$con1{$_}{FULL}      ====> $con1{$_}{COUNT}\n$con2{$_}{FULL}      ====> $con2{$_}{COUNT}\n\n\n";
-		} else {
-			write OUT;
-		}
-		$fails++;
-	}
+    if ( defined $con2{$_} && $con1{$_}{TYPE} eq $con2{$_}{TYPE} ) {
+        my ( $src, $dst );
+        ( $src, $NULL, $dst ) = split /\s+/, $_;
+        unless ( $src =~ /(.*):(\d+)/ ) {
+            $srcIP   = $src;
+            $srcPort = 0;
+        }
+        else {
+            ( $srcIP, $srcPort ) = ( $1, $2 );
+        }
+
+        #if ( !$srcPort || $srcPort eq "->" ) {
+        #	$srcPort = 0;
+        #	$dstIP = $NULL;
+        #	$dstPort = 0;
+        #}
+        unless ( $dst =~ /(.*):(\d+)/ ) {
+            $dstIP   = $dst;
+            $dstPort = 0;
+        }
+        else {
+            ( $dstIP, $dstPort ) = ( $1, $2 );
+        }
+        $proto = $con1{$_}{TYPE};
+        ( $count1, $count2 ) = ( $con1{$_}{COUNT}, $con2{$_}{COUNT} );
+        if ( $Output && $Output eq "raw" ) {
+            print OUT
+              "$_ ($con1{$_}{TYPE}) : $con1{$_}{COUNT} / $con2{$_}{COUNT}\n";
+        }
+        elsif ( $Output && $Output eq "full" ) {
+            print OUT
+"$con1{$_}{FULL}      ====> $con1{$_}{COUNT}\n$con2{$_}{FULL}      ====> $con2{$_}{COUNT}\n\n\n";
+        }
+        else {
+            write OUT;
+        }
+        $fails++;
+    }
 }
 close OUT;
 
-if ( $fails ) {
-	print "There ", $fails > 1 ? "were" : "was" , " a total of $fails failure", $fails > 1 ? "s"  : "", ".  See $outfile for specifics\n";
+if ($fails) {
+    print "There ", $fails > 1 ? "were" : "was", " a total of $fails failure",
+      $fails > 1 ? "s" : "", ".  See $outfile for specifics\n";
 }
 else {
-	print "No common conversations in both instances\n";
+    print "No common conversations in both instances\n";
 }
