14f07309a88cb178352b61db2f799fe07ba97df8
larry <larry>
2009-10-01 11:59:50 +0000

    Remove old sample patches

--- tools/update_support.sh
+++ tools/update_support.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# $Id$
+# $Id: update_support.sh,v 1.1 2009/10/01 11:57:50 larry Exp $
 
 # Must be run from current build (Testing) directory
 
+ssh frodo rm -f /home/www/support_files/auto/files/*SamplePatch*.sh
 cd /nfs/netboot/upgrades/Testing
 cd $(ls -tr | tail -1)
 for file in *SamplePatch*.sh
