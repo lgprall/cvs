3fd435fe013902a3c834883ebf397bcf5fb2d7b5
larry <larry>
2015-08-20 12:56:51 +0000

    Set up to call mysql directly

--- tools/unreg.sh
+++ tools/unreg.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# $Id: $
+# $Id: unreg.sh,v 1.1 2015/08/20 12:49:51 larry Exp $
 
-cat uuids.txt | while read host
+mysql -padmin sfsnort -e 'select uuid from EM_peers where role = 2 and active = 1'  | awk '{print}' | while read host
 do
     date
     echo $host
-    remove_peer.pl $host
+    remove_peer.pl $host 2>/dev/null
 done
 date
