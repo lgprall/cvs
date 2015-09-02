9b483ffadb208b7482ae6690fe6c71f46fac54a1
larry <larry>
2015-09-02 15:11:13 +0000

    Just a simple wrapper; checking in just to have a place to keep it.

---
+++ tools/regall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./reg.sh 4 1 50
+sleep 1800
+./reg.sh 4 51 100
+sleep 1800
+./reg.sh 4 101 150
+sleep 1800
+./reg.sh 4 151 200
+sleep 1800
+./reg.sh 4 201 250
+sleep 1800
+./reg.sh 5 1 50
